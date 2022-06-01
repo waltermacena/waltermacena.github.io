a89b620438bb6620e72d65716b8adc2c80ddf624
Walter Macena <97458939+waltermacena@users.noreply.github.com>
2022-06-01 12:23:46 -0300

    Add files via upload

---
+++ assets/Sidebar.js
@@ -0,0 +1,18 @@
+
+
+var checkBox = document.getElementById("myCheck");
+var groupRotation;
+			function myCheckFunction() {
+			  // Get the checkbox
+			  if ( checkBox.checked == true ){
+				groupRotation = true;
+			  } else {
+			  	groupRotation = false;
+			  }
+				//group.rotation.y += 0.005;
+				//stats.update();
+				 
+			  //animate();
+			}
+export {groupRotation};
+			
